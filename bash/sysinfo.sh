544d297d7b1189015731bf47915679afd04a5b0b
Raitaran08 <200515556@student.georgianc.on.ca>
2023-12-10 20:42:43 -0500

    new adition in sysinfo.sh

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #used the hostname command to get the fully qualified domain name
-echo "FQDN: $(hostname)"
+echo "FQDN: $(hostname -f)"
 
 #displaying host information
 echo "Host Information:"
@@ -16,3 +16,32 @@ ip -o addr show | awk '$3 != "lo" {print $4}'
 echo "Root Filesystem Status:"
 #df is used to tell disk space usage and awk to format and print relevant information for root filesystem
 df -h / | awk 'NR==2 {print "Filesystem: "$1, "Size: "$2, "Used: "$3, "Avail: "$4,"Use%: "$5, "Mounted on: "$6}'
+
+#it shows simple name of the machine
+HOSTNAME=$(hostname)
+
+#it will show qualified name of machine
+FQDN=$(hostname -f)
+
+#it will retrieve operating system as well as its version
+OperatingSystem_Name=$(uname -s)
+OperatingSystem_Version=$(uname -r)
+
+#default ip address is used and awk is used to extract 7th field from output
+IP_Address=$(ip route get 8.8.8.8 | awk '{print$7}')
+
+#it will tell the available space on the root filesystem
+RootSpace=$(df -h / | awk 'NR==2 {print $4}')
+
+cat <<EOF
+
+Report for $HOSTNAME
+====================
+
+Fully Qualified Domain Name: $FQDN
+Operating System: $OperatingSystem_Name $OperatingSystem_Version
+IP Address: $IP_Address
+Root Filesystem Space: $RootSpace
+
+==================
+EOF
